9effc17c53e4b6a7eb2e0c454e6bdc293526f4f6
koizr <koizumi.ryo.dev@gmail.com>
2021-11-24 21:49:41 +0900

    snippet: get i32 from input string as binary

--- src/snippet/input.rs
+++ src/snippet/input.rs
@@ -21,3 +21,11 @@ fn input_vec_i32() -> Vec<i32> {
 
     s.split_whitespace().map(|n| n.parse().unwrap()).collect()
 }
+
+#[allow(dead_code)]
+fn input_i32_as_bits() -> i32 {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+
+    i32::from_str_radix(&s, 2).unwrap()
+}
